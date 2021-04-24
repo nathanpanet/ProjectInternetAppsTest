ec727e9c2ce51e24b782274f6e09f29b61b84668
nathan <natanpanet123@gmail.com>
2021-04-24 12:05:24 +0300

    categories

--- ProjectInternetAppsTest/Startup.cs
+++ ProjectInternetAppsTest/Startup.cs
@@ -50,7 +50,7 @@ namespace ProjectInternetAppsTest
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Categories}/{id?}");
             });
         }
     }
