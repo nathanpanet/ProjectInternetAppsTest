15867be4f868c9362af2dec22df544e2da9e778c
nathan <natanpanet123@gmail.com>
2021-05-07 17:07:54 +0300

    addedOtomaticControllers

---
+++ ProjectInternetAppsTest/Data/ProjectInternetAppsTestContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectInternetAppsTest.Models;
+
+namespace ProjectInternetAppsTest.Data
+{
+    public class ProjectInternetAppsTestContext : DbContext
+    {
+        public ProjectInternetAppsTestContext (DbContextOptions<ProjectInternetAppsTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectInternetAppsTest.Models.Category> Category { get; set; }
+
+        public DbSet<ProjectInternetAppsTest.Models.Order> Order { get; set; }
+
+        public DbSet<ProjectInternetAppsTest.Models.Product> Product { get; set; }
+
+        public DbSet<ProjectInternetAppsTest.Models.User> User { get; set; }
+    }
+}
--- ProjectInternetAppsTest/Startup.cs
+++ ProjectInternetAppsTest/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectInternetAppsTest.Data;
 
 namespace ProjectInternetAppsTest
 {
@@ -24,6 +26,9 @@ namespace ProjectInternetAppsTest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProjectInternetAppsTestContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjectInternetAppsTestContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
